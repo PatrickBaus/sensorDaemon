a7d30238c5b7d09ccd101b7bff92daf273fac8c0
Patrick Baus <patrick.baus@physik.tu-darmstadt.de>
2020-04-30 23:34:17 +0200

    Fixed Postgres insert statement

--- sensorDaemon.py
+++ sensorDaemon.py
@@ -63,7 +63,7 @@ MYSQL_STMS = {
 }
 
 POSTGRES_STMS = {
-    'insert_data' : "INSERT INTO sensor_data (date ,sensor_id ,value) VALUES (NOW(), (SELECT id FROM sensors WHERE sensor_uid=%s and enabled), %s)",
+    'insert_data' : "INSERT INTO sensor_data (time ,sensor_id ,value) VALUES (NOW(), (SELECT id FROM sensors WHERE sensor_uid=%s and enabled), %s)",
 #    'select_period': "SELECT callback_period FROM `sensors` WHERE sensor_uid=%s AND enabled",
 #    'select_hosts' : "SELECT hostname, port FROM `sensor_nodes` WHERE id IN (SELECT DISTINCT node_id FROM `sensors` WHERE enabled)"
 }
