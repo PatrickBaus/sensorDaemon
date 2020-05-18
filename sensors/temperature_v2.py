d022061c6bbb84996eb0703fbaac8987b52d3188
Patrick Baus <patrick.baus@physik.tu-darmstadt.de>
2020-05-18 18:27:41 +0200

    Fixed get_callback_period

--- sensors/temperature_v2.py
+++ sensors/temperature_v2.py
@@ -53,7 +53,7 @@ class TemperatureSensorV2(Sensor):
         """
         Returns the callback period in ms.
         """
-        return self.bricklet.get_temperature_callback_configuration()['period']
+        return self.bricklet.get_temperature_callback_configuration()[0]
 
     @property
     def bricklet(self):
